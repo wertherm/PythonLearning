448bb7c5a2c28a7a50b0637cc6a608b929b2e426
Werther <wertherm@gmail.com>
2021-03-26 14:26:12 -0300

    Atualizacao do enum extendido

--- Basico/enums.py
+++ Basico/enums.py
@@ -4,15 +4,11 @@ class Dias(enum.Enum):
    Dom = 1
    Seg = 2
 
-class Multiple(enum.Enum):
+class EnumMultidimensional(enum.Enum):
    T1 = 1, 'Teste 1'
    T2 = 2, 'Teste 2'
 
-class Nacionalidade(enum.Enum):
-    BR = 0, 'Brasileiro'
-    US = 1, 'Americano'
-    FR = 2, 'Frances'
-
+class EnumExtendido(enum.Enum):
     def __new__(cls, value, name):
         member = object.__new__(cls)
         member._value_ = value
@@ -22,9 +18,14 @@ class Nacionalidade(enum.Enum):
     def __int__(self):
         return self.value
 
+class Nacionalidade(EnumExtendido):
+    BR = 0, 'Brasileiro'
+    US = 1, 'Americano'
+    FR = 2, 'Frances'
+
 print (Dias['Seg'])
 print (Dias(1))
-print (Multiple.T2.value[1])
+print (EnumMultidimensional.T2.value[1])
 
 print()
 
